2bbfb808b7f05b324050062ccb2eee7ecf27e604
mninosua <157538398+mninosua@users.noreply.github.com>
2024-04-03 18:15:53 -0400

    Done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,26 @@
-// This is where you should write all JavaScript
-// for your project. Remember a few things as you start!
-// - Use let or const for all variables
-// - Do not use jQuery - use JavaScript instead
-// - Do not use onclick - use addEventListener instead
-// - Run npm run test regularly to check autograding
-// - You'll need to link this file to your HTML :)
+
+const myName = "Maria";
+const myAge = 22;
+
+console.log(myAge);
+
+// Practical Javascript
+// Show and hide an element on the page
+
+// Find the element to hide
+const toggledParagraph = document.querySelector('.show');
+console.log(toggledParagraph);
+
+// Write a function that toggles the show/hide classes
+
+function toggleParagraph() {
+	toggledParagraph.classList.toggle("show");
+	toggledParagraph.classList.toggle("hide");
+	console.log("My toggle function");
+	console.log(toggledParagraph);
+}
+
+
+// find the button and toggle the paragraph when we click it
+const toggleButton = document.querySelector("#toggle-control");
+toggleButton.addEventListener("click", toggleParagraph);
